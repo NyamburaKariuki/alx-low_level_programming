0e884dd489fd170432add8112d7cd893c1a6d443
NyamburaKariuki <prettimercii@gmail.com>
2023-10-05 15:46:06 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Printing using puts'
+ * Return: Always 0 (Success)
+*/
 int main(void)
-/*code to print using puts*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
