cf2b2c3e0753cb4692967d6caf3e66d0fb66562e
NyamburaKariuki <prettimercii@gmail.com>
2023-10-11 21:22:49 +0300

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,19 +7,32 @@
 
 void times_table(void)
 {
-	int i, j, m;
+	int i, j, k;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j < 10; j++)
 		{
-			m = i * j;
-		_putchar(m + '0');
-		_putchar(',');
-		_putchar(' ');
+			k = i * j;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
 		}
-	
-	_putchar('\n');
+			else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
 	}
 
 }
