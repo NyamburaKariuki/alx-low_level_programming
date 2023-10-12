ba016a1adaa29c38472588b812fde74239bbfa64
NyamburaKariuki <prettimercii@gmail.com>
2023-10-12 12:47:47 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *mul - function to multiply 2 number
+  *@a: parameter being tested
+  *@b: parameter being tested
+  * Return: Always 0 (Success)
+  */
+
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
+
+
