6d6299ec1ef435e69bf8ec0f9b23e7fdad449112
NyamburaKariuki <prettimercii@gmail.com>
2023-10-12 15:06:29 +0300

    5-more.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * more_numbers - function to print number 1 t0 14 10 times
+  * Return: Always 0 (Success)
+  */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+		}
+}
