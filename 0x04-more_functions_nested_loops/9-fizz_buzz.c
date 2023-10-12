0806495f945e6a9462b7eeee83050a3e1308510c
NyamburaKariuki <prettimercii@gmail.com>
2023-10-12 21:24:31 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 5 == 0)
-			printf("Buzz");
-		else if (x % 15 == 0)
+		if (x % 15 == 0)
 			printf("FizzBuzz");
 		else if (x % 3 == 0)
 			printf("Fizz");
+		else if (x % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%d", x);
 		if (x != 100)
