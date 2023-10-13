9bd9fc6df75c0053d65b3c6fa9d71f8e46389172
NyamburaKariuki <prettimercii@gmail.com>
2023-10-13 14:38:41 +0300

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *largest_number - returns the largest of 3 numbers
 *@a: first integer
@@ -25,4 +26,5 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
+
 }
