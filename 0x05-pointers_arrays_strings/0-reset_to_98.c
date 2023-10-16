f50218c210698540932f7b6d38c953ca19161f48
NyamburaKariuki <prettimercii@gmail.com>
2023-10-16 12:13:57 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 - fn takes a pointer to an int and update the value to 98
+*@n: our parameter
+*Return: Always 0 (Success)
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
