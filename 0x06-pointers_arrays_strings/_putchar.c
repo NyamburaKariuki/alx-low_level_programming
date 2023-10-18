a9850a0ee6e4bea3661c347566c8565d5bbd9c1f
NyamburaKariuki <prettimercii@gmail.com>
2023-10-19 00:57:24 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+  *_putchar - Writes the character c to standard output
+  *@c: the character to print
+  *Return: 1 on success
+  *on error, retuns -1 and errno is set appopriately
+  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
