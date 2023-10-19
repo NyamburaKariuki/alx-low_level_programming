121fb0339851f9517131584b8f18ab9ebc3af82c
NyamburaKariuki <prettimercii@gmail.com>
2023-10-19 22:08:15 +0300

    [3-strcmp.c]

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,15 +8,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && (*s1 == *s2))
+	while (*s1 && *s2 && *s1 == *s2)
 	{
-		*s1++;
-		*s2++;
+		s1++;
+		s2++;
 	}
-	if (*s1 == *s2)
-		return (0);
-	else if (*s1 < *s2)
-		return (-1);
-	else
-		return (1);
+return (*s1 - *s2);
 }
