0ff9eba632fdadeeb2d43044924a983e61eb7bcf
NyamburaKariuki <prettimercii@gmail.com>
2023-10-23 20:15:22 +0300

    [100-set_string.c]

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef main_h
 #define main_h
 
+void set_string(char **s, char *to);
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
