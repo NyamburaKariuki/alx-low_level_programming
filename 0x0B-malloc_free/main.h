58aa92de726cb41282d20f3f5a27e9909b1c6017
NyamburaKariuki <prettimercii@gmail.com>
2023-10-31 19:58:27 +0300

    Prototype functions

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+#define nain_h
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+#endif
