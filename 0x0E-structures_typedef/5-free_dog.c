2c9578fcfdeb6d79516ae3b1826818e25c9f85b9
NyamburaKariuki <prettimercii@gmail.com>
2023-11-06 22:59:40 +0300

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+/**
+    *free_dog - function to free dog
+    *@d: pointer to struct dog
+    *Return: Always 0 (Success)
+    */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
