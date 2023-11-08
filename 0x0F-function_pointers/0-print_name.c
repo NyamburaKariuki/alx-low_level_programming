f9e40f71c8c40660509b8a1a14ed804636d599ec
NyamburaKariuki <prettimercii@gmail.com>
2023-11-08 19:39:04 +0300

    putchar file

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_name - function that prints a name
+*@name: name of a person to be printed
+*@f:function pointer
+*Return: Always 0 (Success)
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
