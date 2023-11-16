cbb19eebcc2224809fc54570c3f61d5b9fa436e2
NyamburaKariuki <prettimercii@gmail.com>
2023-11-16 17:29:43 +0300

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void myfucntion(void) __attribute__ ((constructor));
+
+/**
+   *myfunction - prsnts a sentence before the main
+   * function is executed
+   */
+void myfunction(void)
+{
+	 printf("You're beat! and yet, you must allow,\n");
+	  printf("I bore my house upon my back!\n");
+}
+
