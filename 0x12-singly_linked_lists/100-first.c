af13b31f063ab157d909fac538ec78048d7d14a4
NyamburaKariuki <prettimercii@gmail.com>
2023-11-17 10:57:02 +0300

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
-/*
+#include "lists.h"
+/**
  *myfunction - prsnts a sentence before the main
- * function is executed
+ *Description: function is executed
  */
+
 void myfunction(void)
 {
 	 printf("You're beat! and yet, you must allow,\n");
