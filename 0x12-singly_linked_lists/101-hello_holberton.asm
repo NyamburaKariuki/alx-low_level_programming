4721a968c9378e64c97296572ed147a8f0575032
NyamburaKariuki <prettimercii@gmail.com>
2023-11-20 14:49:56 +0300

    a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,6 +1,6 @@
 section .data
-hello:	db "Hello, Holberton", 0
-newline: db 10  ; ASCII code for newline character
+    hello db "Hello, Holberton", 0
+    newline db "%s", 10, 0 ; ASCII code for newline character
 
 section .text
     global main
@@ -8,15 +8,15 @@ section .text
     extern printf
 
 main:
-
-    sub rsp               ; Allocate space for the parameters (Windows-specific)
+    sub rsp, 8                ; Allocate space for the parameters (Windows-specific)
 
     mov rdi, hello            ; Format string
     mov rdx, newline          ; Newline character
     mov rcx, 0                ; Variadic function, no float arguments
     call printf               ; Call printf function
 
-    add rsp               ; Clean up the stack
+    add rsp, 8                ; Clean up the stack
 
     mov eax, 0                ; Return 0 from main
     ret
+
