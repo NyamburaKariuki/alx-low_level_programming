900d14251da53d695c7e3bc98f429d33c4225a06
NyamburaKariuki <prettimercii@gmail.com>
2023-11-24 20:15:04 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  *print_binary - print the the binary representation of an integer
+  *print_binary - print the binary representation of an integer n
   *@n: integer
   *Return: Always 0 (Success)
   */
@@ -9,12 +9,12 @@ void print_binary(unsigned long int n)
 {
 	if (n >> 0)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
+	if (n >> 1)
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 	}
 	else
 	{
-		_putchar('0');
+	_putchar('0');
 	}
 }
