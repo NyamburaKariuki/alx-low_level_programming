c4e1cff1f3792a752265ae51319c9f18fe235607
NyamburaKariuki <prettimercii@gmail.com>
2023-11-28 16:00:48 +0300

    a program that copies the content of a file to another file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,7 @@
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(const char *s);
+void fileclose(int fd);
+char *add_buffer(char *file);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
