4d4b866a172c0847013c614602dba0e02eb2786f
NyamburaKariuki <prettimercii@gmail.com>
2023-12-30 09:40:52 +0300

    elf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -50,3 +50,19 @@ void _readheader(const char *file_name)
         printf("\n");
         close(filedescriptor);
 }
+/**
+  *main - main entry
+  *@argc: number of arguments
+  *@argv: arguments
+  *Return: Always 0 (Success)
+          */
+int main(int argc, char *argv[])
+{
+        if (argc != 2)
+        {
+		fprintf(stderr, "usage: %s elf_filename\n", argv[0]);
+		return (98);
+        }
+	_readheader(argv[1]);
+	return (0);
+}
